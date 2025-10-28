5a3d0d28b605b9e5ffc8af2a85477bd47707ef6a
pranyasachan26 <sachanpa@mail.uc.edu>
2025-10-28 01:21:46 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+public class Main{//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public static void main() {
 
@@ -7,7 +7,7 @@ public static void main() {
     double fallCost = 6000;
     double winterCost = 12000;
     double annualCost = springCost + summerCost + fallCost + winterCost;
-    System.out.println("The cost for Fall " + fallCost " and the cost for Spring " + springCost + " and the cost for Winter " + winterCost + " and the cost for Summer " + summerCost + " is " + annualCost);
+    System.out.println("The cost for Fall " + fallCost + " and the cost for Spring " + springCost + " and the cost for Winter " + winterCost + " and the cost for Summer " + summerCost + " is " + annualCost);
 
 
     }
